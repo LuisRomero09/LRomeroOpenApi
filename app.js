b2bd0c2254ab64d7c5b7bb9b647cedb1d44c9667
LuisRomero09 <luisivancobat@gmail.com>
2024-11-21 21:43:49 -0600

    cb

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import dotenv from 'dotenv';
+import path from 'path';
 
-// Configurar dotenv para cargar variables de entorno
+// Configurar dotenv para cargar variables de entornos
 dotenv.config();
 
 const app = express();
 
 // Cargar la especificación OpenAPI
-// Asegúrate de que el archivo openapi.json esté en la raíz del proyecto o ajusta la ruta según corresponda
-const swaggerDocument = JSON.parse(fs.readFileSync('./openapi.json', 'utf8'));
+let swaggerDocument;
+try {
+  const filePath = path.resolve('openapi.json');
+  swaggerDocument = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+} catch (error) {
+  console.error("Error al leer el archivo openapi.json:", error.message);
+  process.exit(1); // Finalizar la ejecución si no se puede cargar el archivo
+}
 
 // Ruta de la documentación
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -23,9 +30,9 @@ app.get('/api/saludo', (req, res) => {
 // Puerto desde las variables de entorno o por defecto
 const PORT = process.env.PORT || 3000;
 
-// Iniciar el servidorr
+// Iniciar el servidor
 app.listen(PORT, () => {
-  // Cambia los logs para que reflejen la URL correcta en producción
+  // Cambiar los logs para reflejar la URL correcta en producción
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
   console.log(`Documentación en http://localhost:${PORT}/api-docs`);
 });
